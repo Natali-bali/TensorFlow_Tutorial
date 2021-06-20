45fbd7c93cdc0baca95603e38b7e9ce00d0f5d0f
Natalia <65337606+Natali-bali@users.noreply.github.com>
2021-06-20 14:21:29 +0200

    Update helper_functions.py
    Added compare 2 histories plot function

--- helper_functions.py
+++ helper_functions.py
@@ -244,3 +244,46 @@ def load_and_pred(model, path, class_names = classes_names, image_size = (224,22
   img = load_and_prep_image(img_size, path)
   pred = model.predict(img)
   print(f'Prediction: {class_names[pred[0].argmax()] }')
+
+def compare_historys(original_history, new_history, initial_epochs=5):
+  """
+  Compares two model history objects.
+  """
+  # Get original history measurements
+  acc = original_history.history["accuracy"]
+  loss = original_history.history["loss"]
+
+  print(len(acc))
+
+  val_acc = original_history.history["val_accuracy"]
+  val_loss = original_history.history["val_loss"]
+
+  # Combine original history with new history
+  total_acc = acc + new_history.history["accuracy"]
+  total_loss = loss + new_history.history["loss"]
+
+  total_val_acc = val_acc + new_history.history["val_accuracy"]
+  total_val_loss = val_loss + new_history.history["val_loss"]
+
+  print(len(total_acc))
+  print(total_acc)
+
+  # Make plots
+  plt.figure(figsize=(8, 8))
+  plt.subplot(2, 1, 1)
+  plt.plot(total_acc, label='Training Accuracy')
+  plt.plot(total_val_acc, label='Validation Accuracy')
+  plt.plot([initial_epochs-1, initial_epochs-1],
+            plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+  plt.legend(loc='lower right')
+  plt.title('Training and Validation Accuracy')
+
+  plt.subplot(2, 1, 2)
+  plt.plot(total_loss, label='Training Loss')
+  plt.plot(total_val_loss, label='Validation Loss')
+  plt.plot([initial_epochs-1, initial_epochs-1],
+            plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+  plt.legend(loc='upper right')
+  plt.title('Training and Validation Loss')
+  plt.xlabel('epoch')
+  plt.show()
